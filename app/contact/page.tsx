02e59911ef8217f7f230c69a199b2e72d80e9708
kamalsroor1 <teko.2031@gmail.com>
2026-07-18 07:20:48 +0300

    توضيح التعديل الجديد هنا

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { ContactSection } from "@/components/sections/ContactSection";
 
+
 export default function ContactPage() {
   return (
     <>
